dba6aefd309845ee782f4639499254b9568a5a32
rxch1t <naughtymetoday@gmail.com>
2026-04-24 20:45:34 +0530

    Refactor topK function to find top K frequent elements

--- day39.c
+++ day39.c
@@ -26,33 +26,26 @@ Output: [1,2]
 
 
 #include <stdio.h>
-
 void topK(int nums[],int n,int k){
     int freq[1000]={0};
     for(int i=0;i<n;i++){
         freq[nums[i]]++;
     }
-    for(int i=0;i<1000;i++){
-        for(int j=i+1;j<1000;j++){
-            if(freq[i]<freq[j]){
-                int t=freq[i];
-                freq[i]=freq[j];
-                freq[j]=t;
-                int x=i,y=j;
-                int tmp=x;
-                x=y;
-                y=tmp;
+
+    for(int i=0;i<k;i++){
+        int max=0,idx=0;
+
+        for(int j=0;j<1000;j++){
+            if(freq[j]>max){
+            max=freq[j];
+            idx=j;
             }
         }
-    }
-    int count=0;
-    for(int i=0;i<1000 && count<k;i++){
-        if(freq[i]>0){
-            printf("%d ",i);
-            count++;
-        }
+        printf("%d ",idx);
+        freq[idx]=0;
     }
 }
+
 int main(){
     int nums[]={1,1,1,2,2,3};
     int n=6,k=2;
